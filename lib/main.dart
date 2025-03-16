41fa7cd4ac65645fac524b56cd0937ba56806371
hasib3314 <hasibislam2k18@gmail.com>
2025-03-16 09:55:03 +0600

    update setting gradle and controller bug

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weight_scale_v2/auth_controller.dart';
-import 'package:weight_scale_v2/firebase_options.dart';
 import 'package:weight_scale_v2/home_page.dart';
 import 'package:weight_scale_v2/login_page.dart';
 import 'package:weight_scale_v2/product_controller.dart';
@@ -10,12 +9,11 @@ import 'package:weight_scale_v2/product_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(
-      GetMaterialApp(debugShowCheckedModeBanner: false, home: LandingPage()));
+       GetMaterialApp(debugShowCheckedModeBanner: false, home: LandingPage()));
 }
+
 
 class LandingPage extends StatelessWidget {
   final AuthController _authController = Get.put(AuthController());
@@ -24,7 +22,7 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Obx(() {
       if (_authController.user != null) {
-        return HomePage(); // Assuming you have a HomePage widget
+        return HomePage();  // Assuming you have a HomePage widget
       } else {
         return LoginPage();
       }
